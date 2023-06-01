c952cd1aaa659a9f8fe0ec4849e7073e5b678464
Chalas99 <85922129+Chalas99@users.noreply.github.com>
2023-06-01 23:33:23 +0530

    added the routes and signin page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import {Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import HomePage from './pages/HomePage';
+import SignIn from './pages/SignIn';
 
 function App() {
   return (
     <div className="App">
-      
+        <Routes>
+            <Route path="/" element={<Layout/>}>
+              <Route path="/" element={<HomePage/>}></Route>
+              <Route path="/Login" element={<SignIn/>}></Route>
+            </Route>
+        </Routes>
     </div>
   );
 }
